88219d75f0516c07eeda75454905f308b7e1e4d8
Aryanand Arnav <aryanandarnav13@gmail.com>
2021-07-25 19:03:36 +0530

    style : done with styling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var outputDiv = document.querySelector("#output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text) {
-    return serverURL + "?" + "text=" + text
+    return serverURL + "?" + "text=" + text;
 }
 
 function errorHandler(error) {
